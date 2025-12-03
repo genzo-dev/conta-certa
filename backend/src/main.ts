ceca364db021f28d9160ca9e4e495e02183e6481
genzo-dev <gabrielenzopdf12@outlook.com>
2025-12-03 10:55:21 -0300

    docs: add swagger and swagger configuration on main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const documentBuildConfig = new DocumentBuilder()
+    .setTitle('Conta Certa - API')
+    .setDescription(
+      'Registre suas transações financeiras e veja como seu dinheiro está sendo utilizado.',
+    )
+    .setVersion('0.01')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuildConfig);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.BACKEND_PORT ?? 3001);
 }
 bootstrap();
