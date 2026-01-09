5f15dbbe4e4dd61a35547223b861a01f3a13cbc5
genzo-dev <gabrielenzopdf12@outlook.com>
2026-01-09 14:42:06 -0300

    refactor(nav): display only the user's first two names

--- frontend/app/components/Nav/index.tsx
+++ frontend/app/components/Nav/index.tsx
@@ -7,7 +7,9 @@ export default async function Nav() {
   return (
     <nav className="flex items-center justify-between bg-[#e3e3e3] shadow-lg px-4 sm:px-6 md:px-12 py-2 md:py-4">
       <Logo mode="nav" />
-      <strong className="">{user?.userName}</strong>
+      <strong className="text-sm sm:text-base md:text-lg lg:text-xl">
+        Olá, {user?.userName?.split(" ").slice(0, 2).join(" ")}
+      </strong>
     </nav>
   );
 }
